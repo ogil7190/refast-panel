aabe4b6f1ca694a0e8426375b2628380e66536ee
Vivek Kumar <vivekk@tekion.com>
2022-04-16 18:54:53 +0530

    minor changes

--- src/views/dashboard/Dashboard-new.js
+++ src/views/dashboard/Dashboard-new.js
@@ -2,18 +2,27 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { GET_STATS_URL } from 'src/urls'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
+import { CSpinner } from '@coreui/react'
 
 const Dashboard = () => {
   const [matrices, setMatrices] = useState([])
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
-    axios.get(GET_STATS_URL).then((resp) => {
-      if (resp.data && resp.data.success) {
-        setMatrices(resp.data.data)
-      }
-    })
+    setLoading(true)
+    axios
+      .get(GET_STATS_URL)
+      .then((resp) => {
+        if (resp.data && resp.data.success) {
+          setMatrices(resp.data.data)
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
   return (
     <>
+      {loading && <CSpinner component="span" size="sm" variant="grow" aria-hidden="true" />}
       <WidgetsDropdown matrices={matrices} />
     </>
   )
