90f9c56211dc34aa969680fd3a5a963558cf0ce2
Vivek Kumar <vivekk@tekion.com>
2022-04-21 09:55:25 +0530

    minor changes

--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,12 @@
-export const HOST_URL = 'http://165.232.182.69:4000'
+const getHost = () => {
+  const currDate = new Date().getDate()
+  if (currDate < 15) {
+    return 'https://refastapp-backend.herokuapp.com'
+  } else {
+    return 'https://refastapp-bckend.herokuapp.com'
+  }
+}
+export const HOST_URL = getHost()
 export const GET_STATS_URL = `${HOST_URL}/panel/stats`
 export const GET_ORDERS_URL = `${HOST_URL}/panel/allOrders`
 export const GET_USERS_URL = `${HOST_URL}/panel/allUsers`
